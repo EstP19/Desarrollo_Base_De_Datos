9da59d5f3af4c69f0b0379b598f1c87f942e99a5
Esteban Pantoja <estebanp192001@gmail.com>
2024-09-05 23:06:35 -0500

    First Commit

---
+++ Backups/Bd_Prueba.sql
@@ -0,0 +1,72 @@
+#Creacion de la base de datos
+#Paso 1
+drop database BD_inventario;
+CREATE DATABASE BD_inventario;
+
+#Paso 2
+
+USE BD_inventario;
+
+#Creacion de las tablas
+#Paso 3
+
+CREATE TABLE type_document(
+idtype_document INT PRIMARY KEY AUTO_INCREMENT,
+type_document VARCHAR(2)
+);
+
+CREATE TABLE employees (
+id INT PRIMARY KEY AUTO_INCREMENT,
+name_ VARCHAR(100),
+createdAt DATETIME,
+updateAt DATETIME,
+lastname VARCHAR(100),
+birthday DATE,
+email VARCHAR(100) UNIQUE,
+phone VARCHAR(20),
+address VARCHAR(255),
+occupation VARCHAR(100),
+date_recruitment DATE,
+document INT UNIQUE,
+idtype_document INT,
+FOREIGN KEY (idtype_document) references type_document(idtype_document)
+);
+
+CREATE TABLE customers (
+    idcustomers INT PRIMARY KEY AUTO_INCREMENT,
+    name_ VARCHAR(100) NOT NULL,
+    lastname VARCHAR(100),
+    email VARCHAR(100) UNIQUE,
+    phone VARCHAR(20),
+    birthday DATE,
+    createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updatedAt DATETIME ON UPDATE CURRENT_TIMESTAMP,
+    document INT UNIQUE,
+    idtype_document INT,
+    FOREIGN KEY (idtype_document) references type_document(idtype_document)
+);
+
+
+CREATE TABLE reserves (
+    idreserves INT PRIMARY KEY AUTO_INCREMENT,
+    state VARCHAR(100) NOT NULL,
+    time_ VARCHAR(20),
+    price VARCHAR(100) ,
+    date_reserve DATE,
+    createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updatedAt DATETIME ON UPDATE CURRENT_TIMESTAMP,
+	idcustomers INT,
+    FOREIGN KEY (idcustomers) REFERENCES customers(idcustomers)
+);
+
+CREATE TABLE payment (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    pay_method VARCHAR(50),
+    date_pay DATE,
+    price INT,
+    idreserves INT,
+    FOREIGN KEY (idreserves) REFERENCES reserves(idreserves)
+);
+
+
+
